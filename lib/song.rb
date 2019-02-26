dce15ef0aeacf24813940a44762c642f8fafb684
Marci Clark <marcig612@gmail.com>
2019-02-26 02:51:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,7 +14,7 @@ class Song
   def self.new_by_filename(file)
     song_name = file.split(" - ")[1]
     artist_name = file.split (" - ")[0]
-    song = self.new(song_name)
+    song = self.new = artist 
     song 
   end 
   
